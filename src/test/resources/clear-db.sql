481791e3a4459cf2b4ecf67e9bac2cdb6d6a6a8a
VladyslavBabenko <babenkovladyslav1@gmail.com>
2022-08-23 01:07:13 +0300

    Added Course entity

--- src/test/resources/clear-db.sql
+++ src/test/resources/clear-db.sql
@@ -1,12 +1,14 @@
 DELETE
-FROM t_user_roles;
-DELETE
 FROM t_article_users;
+DELETE
+FROM t_user_roles;
 DELETE
 FROM t_event_users;
 DELETE
-FROM t_user;
+FROM t_course;
 DELETE
 FROM t_event;
 DELETE
 FROM t_article;
+DELETE
+FROM t_user;
