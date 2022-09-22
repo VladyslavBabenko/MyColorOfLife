f7efd9078635119c111b50db2bac63802d0d5644
VladyslavBabenko <babenkovladyslav1@gmail.com>
2022-09-22 21:58:47 +0300

    Updated Course

--- src/main/java/com/github/vladyslavbabenko/mycoloroflife/entity/Course.java
+++ src/main/java/com/github/vladyslavbabenko/mycoloroflife/entity/Course.java
@@ -3,10 +3,14 @@ package com.github.vladyslavbabenko.mycoloroflife.entity;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
 /**
- * Article entity.
+ * Course entity.
  */
 
 @Builder
@@ -21,19 +25,29 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Integer id;
-    @NotEmpty(message = "Назва не повинна бути порожньою")
-    @Size(min = 1, max = 100, message = "Назва має бути від 1 до 100 символів")
-    @Column(length = 100, nullable = false)
-    private String courseTitle;
+
+    @ManyToOne
+    @JoinColumn(name = "course_title_id", nullable = false)
+    @NotNull(message = "Вкажіть назву")
+    private CourseTitle courseTitle;
+
     @Column(nullable = false)
     @Max(value = 100, message = "Сторінка має бути від 1 до 100")
     @Min(value = 1, message = "Сторінка має бути від 1 до 100")
     @NotNull(message = "Вкажіть сторінку")
     private Integer page;
+
     @Size(max = 100, message = "Назва має бути до 100 символів")
     @Column(length = 100)
     private String videoTitle;
+
     private String videoLink;
+
     @Size(max = 65535, message = "Текст має бути до 65535 символів")
     private String text;
+
+    @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "course_id")
+    @ToString.Exclude
+    private Set<CourseProgress> courseProgresses;
 }
