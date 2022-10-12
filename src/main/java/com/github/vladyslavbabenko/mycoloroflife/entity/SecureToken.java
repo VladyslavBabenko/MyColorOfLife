bd859675b58a9a19cae694d417bb7fc51a27f137
VladyslavBabenko <babenkovladyslav1@gmail.com>
2022-10-12 23:58:41 +0300

    SecureToken Added

---
+++ src/main/java/com/github/vladyslavbabenko/mycoloroflife/entity/SecureToken.java
@@ -0,0 +1,55 @@
+package com.github.vladyslavbabenko.mycoloroflife.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity(name = "t_secure_token")
+public class SecureToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp timeStamp;
+
+    @Column(updatable = false)
+    private LocalDateTime expireAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public boolean isExpired() {
+        return getExpireAt().isBefore(LocalDateTime.now());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        SecureToken that = (SecureToken) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
