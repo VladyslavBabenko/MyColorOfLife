6d22e4b0fd93b2864385a382ee5ee1f82e41b49b
VladyslavBabenko <babenkovladyslav1@gmail.com>
2022-10-15 00:01:58 +0300

    BruteForceProtectionService added

---
+++ src/main/java/com/github/vladyslavbabenko/mycoloroflife/service/BruteForceProtectionService.java
@@ -0,0 +1,28 @@
+package com.github.vladyslavbabenko.mycoloroflife.service;
+
+import com.github.vladyslavbabenko.mycoloroflife.entity.User;
+
+public interface BruteForceProtectionService {
+    /**
+     * Method to register each failed login attempt for the given username.
+     *
+     * @param username {@link User}'s username
+     */
+    void registerLoginFailure(String username);
+
+    /**
+     * Method to reset the counter for successful login.
+     *
+     * @param username {@link User}'s username
+     */
+    void resetBruteForceCounter(String username);
+
+    /**
+     * Method to check if a user account is under brute force attack,
+     * this will compare the number of failed attempts against a threshold.
+     *
+     * @param username {@link User}'s username
+     * @return true if the number of failed attempts exceeds the threshold
+     */
+    boolean isBruteForceAttack(String username);
+}
